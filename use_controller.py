f70f15fb23af7b4abea528a1b3ed56db25f6f9b3
diesel701 <diesel701@gmail.com>
2020-05-07 18:40:26 +0000

    cleanedup a bit

--- use_controller.py
+++ use_controller.py
@@ -91,38 +91,6 @@ elif (controllerName == 'PS4_controller'):
 
 logger = logging.getLogger(__name__)
 
-"""Emulates Switch controller. Opens joycontrol.command_line_interface to send button commands and more.
-
-While running the cli, call "help" for an explanation of available commands.
-
-Usage:
-    run_controller_cli.py <controller> [--device_id | -d  <bluetooth_adapter_id>] 
-                                       [--spi_flash <spi_flash_memory_file>]
-                                       [--reconnect_bt_addr | -r <console_bluetooth_address>]
-                                       [--log | -l <communication_log_file>]
-    run_controller_cli.py -h | --help
-
-Arguments:
-    controller      Choose which controller to emulate. Either "JOYCON_R", "JOYCON_L" or "PRO_CONTROLLER"
-
-Options:
-    -d --device_id <bluetooth_adapter_id>   ID of the bluetooth adapter. Integer matching the digit in the hci* notation
-                                            (e.g. hci0, hci1, ...) or Bluetooth mac address of the adapter in string 
-                                            notation (e.g. "FF:FF:FF:FF:FF:FF").
-                                            Note: Selection of adapters may not work if the bluez "input" plugin is
-                                            enabled.
-
-    --spi_flash <spi_flash_memory_file>     Memory dump of a real Switch controller. Required for joystick emulation.
-                                            Allows displaying of JoyCon colors.
-                                            Memory dumbs can be created using the dump_spi_flash.py script.
-                                            
-    -r --reconnect_bt_addr <console_bluetooth_address>  Previously connected Switch console Bluetooth address in string 
-                                                        notation (e.g. "FF:FF:FF:FF:FF:FF") for reconnection.
-                                                        Does not require the "Change Grip/Order" menu to be opened,
-    
-    -l --log <communication_log_file>       Write hid communication (input reports and output reports) to a file.
-"""
-
 
 def lerp(a, b, percentage):
     return (percentage * a) + ((1 - percentage) * b)
